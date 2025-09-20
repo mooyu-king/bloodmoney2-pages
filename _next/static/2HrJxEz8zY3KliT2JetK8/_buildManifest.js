a8c748d930d48b0739795cd8b5dc56501cf11b1f
mooyuking <mooyuking@gmail.com>
2025-09-20 23:43:50 +0800

    add-normal-math

--- _next/static/2HrJxEz8zY3KliT2JetK8/_buildManifest.js
+++ _next/static/2HrJxEz8zY3KliT2JetK8/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/2c5e020da125e453.js"
+    "./static/chunks/26b81dab389610af.js"
   ],
   "__rewrites": {
     "afterFiles": [],
--- _next/static/chunks/5d8b4125e89667e6.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/488d5200d92c709c.js",
-  "static/chunks/turbopack-4a83d9295540c2f5.js"
-])
