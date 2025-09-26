07463e43521d198068f7e6fccea3248fe7b68023
mooyuking <mooyuking@gmail.com>
2025-09-26 23:46:57 +0800

    18-amend

---
+++ _next/static/chunks/667281184e91bbad.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/a667fda1a8a27e1e.js",
+  "static/chunks/turbopack-46a7ec5e9ee64282.js"
+])
--- _next/static/fM6436nwyMsI_khwf-P-P/_buildManifest.js
+++ _next/static/fM6436nwyMsI_khwf-P-P/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/4085528889f21ed3.js"
+    "./static/chunks/667281184e91bbad.js"
   ],
   "__rewrites": {
     "afterFiles": [],
