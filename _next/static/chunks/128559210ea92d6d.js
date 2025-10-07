c9203ab5d74ab229f9c9a50eccd371c76abcafd7
mooyuking <mooyuking@gmail.com>
2025-10-08 00:35:09 +0800

    add-scarlet-shift

--- _next/static/chunks/128559210ea92d6d.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/3dbfeb1389f4eae1.js",
-  "static/chunks/turbopack-8a499b619b51a0d9.js"
-])
--- _next/static/chunks/d0275400bf3d9d66.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/3dbfeb1389f4eae1.js",
-  "static/chunks/turbopack-a082c0044af87fc5.js"
-])
